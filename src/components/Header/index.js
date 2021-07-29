e8568377d4ebcdc6e88db76df754173fdd3a33f0
danielarevalo <danielarevalo100@gmail.com>
2021-07-29 19:04:03 -0300

    adding menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router'
+import { Collapse } from '@material-ui/core';
 
 /* components */
 import Div from 'components/Div';
@@ -15,21 +16,42 @@ const routes = [
   { path: '/news', title: 'News' },
   { path: '/contact', title: 'Contact' },
 ]
+
+const isMobile = window.innerWidth < 1024;
+
 const Header = (props) => {
+
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <Div className='header-container' >
-      <Div className='header-mobile' style={{ width: '25rem' }} align='start' data-aos='fade-right'>
-        <Div className='mb-3'>
-          <h1>DEAS</h1>
+      {!isMobile && (
+        <Div className='header-mobile' style={{ width: '25rem' }} align='start' data-aos='fade-right'>
+          <Div className='mb-3'>
+            <h1>DEAS</h1>
+          </Div>
+          <Div className='nav mb-3 nav-mobile' align='start'>
+            {routes.map( ({ path, title }) => (<span style={{ color: props.location.pathname == path && '#000000' || '' }} onClick={() => props.history.push(path)} key={path} className='menu-item mb-2'>{title}</span>) )}
+          </Div>
+          <Div className='' align='start' >
+            <span style={{ fontStyle: 'italic', color: '#999999' }}>&#169; 2021 Deas</span>
+            <span style={{ fontStyle: 'italic', color: '#999999' }}>Created by Daniel Arevalo</span>
+          </Div>
         </Div>
-        <Div className='nav mb-3 nav-mobile' align='start'>
-          {routes.map( ({ path, title }) => (<span style={{ color: props.location.pathname == path && '#000000' || '' }} onClick={() => props.history.push(path)} key={path} className='menu-item mb-2'>{title}</span>) )}
+      ) || (
+        <Div>
+          <Div onClick={() => setShowMenu(!showMenu)} className='mb-3'>
+            <h1>DEAS</h1>
+          </Div>
+          <Div style={{ width: '100%' }}>
+            <Collapse in={showMenu}>
+              <Div column>
+                {routes.map( ({ path, title }) => (<span style={{ color: props.location.pathname == path && '#000000' || '' }} onClick={() => props.history.push(path)} key={path} className='menu-item mb-2'>{title}</span>) )}
+              </Div>
+            </Collapse>
+          </Div>
         </Div>
-        <Div className='' align='start' >
-          <span style={{ fontStyle: 'italic', color: '#999999' }}>&#169; 2021 Deas</span>
-          <span style={{ fontStyle: 'italic', color: '#999999' }}>Created by Daniel Arevalo</span>
-        </Div>
-      </Div>
+      )}
 
     </Div>
 
